a7874417263ce434ee4a77597cb685810e5c6246
kamrul <kamrul1728@cseku.ac.bd>
2021-01-22 13:01:08 +0600

    react basics completed

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = (props) =>{
+        return ( 
+            <header>
+                <div className="logo">Logo</div>
+                <input 
+                    type="text" 
+                    onChange={props.keywords}
+                />
+            </header>
+        )
+}
+
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./css/styles.css"
+import JSON from './db.json';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// COMPONENTS
+import Header from './components/header';
+import NewsList from  './components/news_list';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+
+    state = {
+        news:JSON,
+        filtered:[]
+    }
+
+    getKeyword = (event) => {
+        //console.log(event.target.value)
+        let keyword = event.target.value;
+        let filtered = this.state.news.filter((item)=>{
+            return item.title.indexOf(keyword) > -1
+        });
+        this.setState({
+            filtered
+        })
+       // console.log(filtered)
+    }
+
+    render(){
+        let newsFiltered = this.state.filtered;
+        let newsWhole = this.state.news
+        return (
+            <div>
+                 <Header keywords={this.getKeyword}/>
+                 <NewsList news={newsFiltered.length === 0 ? newsWhole : newsFiltered}>
+                    <h3>
+                        The news are:
+                    </h3>
+                 </NewsList>
+            </div>
+        )
+    }  
+}
+
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
